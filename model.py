5beb3e7d1b86c80ba93f97d55c200a13ef702b07
DJSwagThor <thor1458@hotmail.dk>
2025-01-29 14:12:08 +0100

    Update

--- model.py
+++ model.py
@@ -1,17 +1,17 @@
 import torch
 import torch.nn as nn
+from torch.nn import functional as F
 from config import *
-import torch.nn.functional as F
 
 class Head(nn.Module):
     """ one head of self-attention """
 
-    def __init__(self, n_embd, head_size, block_size):
+    def __init__(self, head_size):
         super().__init__()
-        self.key = nn.Linear(n_embd, head_size, bias=False)
-        self.query = nn.Linear(n_embd, head_size, bias=False)
-        self.value = nn.Linear(n_embd, head_size, bias=False)
-        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        self.key = nn.Linear(N_EMBD, head_size, bias=False)
+        self.query = nn.Linear(N_EMBD, head_size, bias=False)
+        self.value = nn.Linear(N_EMBD, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(BLOCK_SIZE, BLOCK_SIZE)))
 
         self.dropout = nn.Dropout(DROPOUT)
 
@@ -32,11 +32,11 @@ class Head(nn.Module):
 class MultiHeadAttention(nn.Module):
     """ multiple heads of self-attention in parallel """
 
-    def __init__(self, n_embd, n_heads, head_size, block_size, dropout):
+    def __init__(self, num_heads, head_size):
         super().__init__()
-        self.heads = nn.ModuleList([Head(n_embd, head_size, block_size) for _ in range(n_heads)])
-        self.proj = nn.Linear(n_embd, n_embd)
-        self.dropout = nn.Dropout(dropout)
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(N_EMBD, N_EMBD)
+        self.dropout = nn.Dropout(DROPOUT)
 
     def forward(self, x):
         out = torch.cat([h(x) for h in self.heads], dim=-1)
@@ -46,13 +46,13 @@ class MultiHeadAttention(nn.Module):
 class FeedFoward(nn.Module):
     """ a simple linear layer followed by a non-linearity """
 
-    def __init__(self, n_embd, dropout):
+    def __init__(self, n_embd):
         super().__init__()
         self.net = nn.Sequential(
             nn.Linear(n_embd, 4 * n_embd),
             nn.ReLU(),
             nn.Linear(4 * n_embd, n_embd),
-            nn.Dropout(dropout),
+            nn.Dropout(DROPOUT),
         )
 
     def forward(self, x):
@@ -61,12 +61,12 @@ class FeedFoward(nn.Module):
 class Block(nn.Module):
     """ Transformer block: communication followed by computation """
 
-    def __init__(self, n_embd, n_heads, block_size, dropout):
+    def __init__(self, n_embd, n_head):
         # n_embd: embedding dimension, n_head: the number of heads we'd like
         super().__init__()
-        head_size = n_embd // n_heads
-        self.sa = MultiHeadAttention(n_embd, n_heads, head_size, block_size, dropout)
-        self.ffwd = FeedFoward(n_embd, dropout)
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedFoward(n_embd)
         self.ln1 = nn.LayerNorm(n_embd)
         self.ln2 = nn.LayerNorm(n_embd)
 
@@ -76,11 +76,11 @@ class Block(nn.Module):
         return x
     
 class PositionalEncoding(nn.Module):
-    def __init__(self, n_embd, max_len=5000, device="cuda"):
+    def __init__(self, d_model, max_len=5000, device="cuda"):
         super(PositionalEncoding, self).__init__()
-        self.encoding = torch.zeros(max_len, n_embd).to(device)
+        self.encoding = torch.zeros(max_len, d_model).to(device)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1).to(device)
-        div_term = torch.exp(torch.arange(0, n_embd, 2).float() * (-torch.log(torch.tensor(10000.0)) / n_embd)).to(device)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-torch.log(torch.tensor(10000.0)) / d_model)).to(device)
         self.encoding[:, 0::2] = torch.sin(position * div_term)
         self.encoding[:, 1::2] = torch.cos(position * div_term)
         self.encoding = self.encoding.unsqueeze(0)
@@ -89,16 +89,16 @@ class PositionalEncoding(nn.Module):
         return x + self.encoding[:, :x.size(1)].detach()
 
 class Transformer(nn.Module):
-    def __init__(self, vocab_size, n_embd, n_layer, n_heads, block_size, dropout, device):
+    def __init__(self):
         super().__init__()
         # Embedding layers for tokens and positions
-        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
-        self.positional_encoding = PositionalEncoding(n_embd, block_size, device)
+        self.token_embedding_table = nn.Embedding(VOCAB_SIZE, N_EMBD)
+        self.positional_encoding = PositionalEncoding(N_EMBD, BLOCK_SIZE, DEVICE)
 
         # Transformer blocks
-        self.blocks = nn.Sequential(*[Block(n_embd, n_heads, block_size, dropout) for _ in range(n_layer)])
-        self.ln_f = nn.LayerNorm(n_embd)  # final layer norm
-        self.lm_head = nn.Linear(n_embd, vocab_size)
+        self.blocks = nn.Sequential(*[Block(N_EMBD, n_head=N_HEAD) for _ in range(N_LAYER)])
+        self.ln_f = nn.LayerNorm(N_EMBD)  # final layer norm
+        self.lm_head = nn.Linear(N_EMBD, VOCAB_SIZE)
 
     def forward(self, idx, targets=None):
         B, T = idx.shape
